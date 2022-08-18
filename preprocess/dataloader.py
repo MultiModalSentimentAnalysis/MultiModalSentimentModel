c50277973fba17a38fda0b4009942f9198befb79
Amin Kashiri <kashiri.amin@gmail.com>
2022-08-18 12:56:44 +0430

    Update dataloader

--- preprocess/dataloader.py
+++ preprocess/dataloader.py
@@ -2,29 +2,18 @@ from torch.utils.data import DataLoader
 from datasets import MSCTDDataSet
 
 
+
 class MSCTDDataLoader:
-    def __init__(self, dl, device, tokenizer=None, text_len=512):
+    def __init__(self, dl, device):
         self.dl = dl
         self.device = device
-        self.tokenizer = tokenizer
-        self.text_len = text_len
 
     def __iter__(self):
         for b in self.dl:
-            if self.tokenizer:
-                b["text"] = self.tokenizer(
-                    b["text"],
-                    padding="max_length",
-                    max_length=self.text_len,  # including [CLS] end [SEP]
-                    truncation=True,
-                    return_tensors="pt",
-                    # return_offsets_mapping=True,
-                )
             yield to_device(b, self.device)
 
     def __len__(self):
         return len(self.dl)
-
 
 def to_device(data, device):
     if isinstance(data, (list, tuple)):
@@ -35,25 +24,16 @@ def to_device(data, device):
         return data
     return data.to(device)
 
-
 if __name__ == "__main__":
-    batch_size = 2
-    num_workers = 1
-
-    from transformers import AutoTokenizer
-
+    batch_size = 10
     device = "cuda"
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-    # tokenizer = None
-    dataset = MSCTDDataSet("data/", "val")
-    dataloader = DataLoader(dataset, batch_size=batch_size)
-    dataloader = MSCTDDataLoader(dataloader, device, tokenizer)
-    # train_loader = DataLoader(ds, batch_size=batch_size, num_workers=num_workers, pin_memory=True) # WHY THIS TAKES DAYS?
-    for x in dataloader:
-        print(x["images"].shape)
-        if tokenizer:
-            print(x["text"]["input_ids"].shape)
-        else:
-            print(x["text"])
-        print(x["sentiment"].shape)
-        break
+    ds = MSCTDDataSet(base_path="data/", dataset_type = "val", load=True)
+    ds.save_features()
+    ds.load_data()
+    dl = DataLoader(ds, batch_size=batch_size)
+    dl = MSCTDDataLoader(dl, device)
+    for x in dl:
+        print(x)
+        print(x['face_embedding'].shape)
+        print(x['text_embedding'].shape)
+        print(x['real_index'])
